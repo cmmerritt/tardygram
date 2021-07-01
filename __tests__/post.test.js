c565af96f8fd178fbfefce6e2e1b25aedb2cc4f6
cmmerritt <christiane.merritt@gmail.com>
2021-06-30 22:14:54 -0700

    make post test passes

--- __tests__/post.test.js
+++ __tests__/post.test.js
@@ -1,18 +1,46 @@
-import { agent } from "supertest";
+import request from 'supertest';
+import app from '../lib/app.js';
+import pool from '../lib/utils/pool.js';
+import setup from '../data/setup.js';
+import UserService from '../lib/services/UserService.js';
 
 describe('post routes', () => {
 
+  let agent;
+  let user = {};
+
+  beforeEach(async () => {
+    await setup(pool);
+    agent = request.agent(app);
+    user = await UserService.create({
+      username: 'stonky',
+      password: 'stonks',
+      profilePhotoUrl: 'https://slate.com/business/2021/01/stonks-not-stocks-got-it.html'
+    });
+    await agent.post('/api/v1/auth/login')
+      .send({
+        username: 'stonky',
+        password: 'stonks'
+      });
+  });
+
   it('creates a post via POST', async () => {
+
     const res = await agent
       .post('/api/v1/posts')
       .send({
-        text: 'My embarrassing LiveJournal post circa 2003'
+        userId: user.id,
+        photoUrl: 'stonkphoto',
+        caption: 'My embarrassing LiveJournal post circa 2003',
+        tags: ['wow', 'stonk', 'stronk']
       });
 
     expect(res.body).toEqual({
       id: '1',
-      userId: '1',
-      text: 'My embarrassing LiveJournal post circa 2003'
+      userId: user.id,
+      photoUrl: 'stonkphoto',
+      caption: 'My embarrassing LiveJournal post circa 2003',
+      tags: ['wow', 'stonk', 'stronk']
     });
   });
 });
